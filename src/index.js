a22f5b60e66c77a238d88dab013ae278eba8167c
Mike Zheng <mike.zheng@mohawkcollege.ca>
2016-10-29 16:37:48 -0400

    final lesson state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyDksPW1h41xcv_eDOs7u8vzHLRbSPSRAXs';
+
 
 // create a new component
 // produces html
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const App = () => {
-  return <div>Hi!</div>;
+    this.state = {
+      videos: [] ,
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo:videos[0]
+      });
+    });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300)
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect= {(selectedVideo) => this.setState({selectedVideo})}
+        videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 
